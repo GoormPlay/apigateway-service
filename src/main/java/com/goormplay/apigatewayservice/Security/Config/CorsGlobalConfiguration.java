8ae686c44fb0f0ee79889f4cdfe56e481d8393ad
ashenglow <158246901+ashenglow@users.noreply.github.com>
2025-05-30 14:51:15 +0900

    Feature/test jenkins (#53)
    * fix: modify yml
    
    * fix: route
    
    * fix: modify script
    
    * Update run-all.sh
    
    * feat:add CorsWebFilter

--- src/main/java/com/goormplay/apigatewayservice/Security/Config/CorsGlobalConfiguration.java
+++ src/main/java/com/goormplay/apigatewayservice/Security/Config/CorsGlobalConfiguration.java
@@ -1,13 +1,33 @@
 package com.goormplay.apigatewayservice.Security.Config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 import org.springframework.web.reactive.config.CorsRegistry;
 import org.springframework.web.reactive.config.WebFluxConfigurer;
 import org.springframework.web.reactive.config.WebFluxConfigurerComposite;
 
 @Configuration
 public class CorsGlobalConfiguration {
+    @Value("${cors.allowed-origin}")
+    private String allowedOrigin;
+
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin(allowedOrigin); // Jenkins 환경변수에서 전달됨
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsWebFilter(source);
+    }
     @Bean
     public WebFluxConfigurer corsConfigurer() {
         return new WebFluxConfigurerComposite() {
@@ -20,4 +40,6 @@ public class CorsGlobalConfiguration {
             }
         };
     }
+
+
 }
